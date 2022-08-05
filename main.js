a3370d29ea56f11f13966f76478be4141dcac5e9
Ignacio <ignacio.becher@gmail.com>
2022-08-04 23:35:34 -0300

    ggg

--- main.js
+++ main.js
@@ -381,6 +381,33 @@ course.abandonar=function(){console.log("abandonar")}
 
 //----------------------------CONCEPTOS DE PROTOTIPOS-------------------//
 
+// Conceptos de prototipos
 
+
+
+// Tenemos la propiedad prototype, todas las funciones tienen esta propiedad y apunta al prototype object.
+// El prototype object es el objeto al que podemos asignar métodos y propiedades que queremos compartir en el prototype chain o cadena de prototipos.
+// Cuando creamos un objeto de una función usando new, se asigna una propiedad proto que apunta al prototype de la función con la que se creó el objeto, también conocida como función constructora. Esto quiere decir que podemos decir que proto apunta al prototype de la función constructora.
+// El prototype chain o la cadena de prototipos hace referencia al enlace que se crea entre prototipos que tienen su propio prototipo, que tienen su propio prototipo. Cuando buscamos un método o una propiedad en un objeto, se buscará inicialmente en el objeto y luego en la cadena de prototipos.
+// El último eslabón de la cadena de prototipos es Object.prototype, este objeto es usado para definir métodos que compartan todos los objetos del lenguaje, ya que eventualmente la cadena de prototipos termina aquí.
+// El prototipo de una función constructora apunta por defecto al prototype de Object, aunque esto puede ser modificado.
+// Un dato interesante es que cualquier objeto creado con la notación JSON enlaza su propiedad proto al prototype de la función constructora Object.
+
+//----------------------------PROTOTIPOS EN LA PRACTICA-------------------//
+
+//El objeto prototype de una funcion es un espacio para definir elementos que se compartiran entre todos los objetos creados a partir de la funcion
+
+function Course(){}
+
+Course.prototype.inscribir=function(){
+   console.log("ok")
+}
+
+let JavaScript= new Course()
+let ruby= new Course()   //ambos imprimen ----> "ok" 
+//estas nuevas instancias heredan el protoype, los elementos heredados son compartidos, no copiados
+
+
+//----------------------------HERENCIA DE PROTOTIPOS-------------------//
 
 //HACER UN PUSH AL FINALIZAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*******************
